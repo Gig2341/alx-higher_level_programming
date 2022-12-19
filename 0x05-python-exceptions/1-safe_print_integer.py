d10544fbebb7d506699113e9c70bd1aab675012c
Gig2341 <danmata2341@gmail.com>
2022-12-19 22:03:23 +0000

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
 	try:
-		if value == int:
-			return True
-	except TypeError:
-		break
-	
+		print("{:d}".format(value))	
+		return True
+	except (TypeError,ValueError):
+		return False
