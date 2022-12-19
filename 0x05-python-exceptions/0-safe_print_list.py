58aceebc8ee17661e936359265da6c5ede474b68
Gig2341 <danmata2341@gmail.com>
2022-12-19 10:31:02 +0000

    readme created

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0)
+	'''a function that prints x elements of a list.
+	my_list can contain any type (integer, string, etc.)
+	x represents the number of elements to print
+	x can be bigger than the length of my_list
+	Returns the real number of elements printed'''
+
+	try:
+		if my_list
