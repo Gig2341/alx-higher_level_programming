b2d59f90d1a40599ffdcb3fb28c6bf6a0ec05656
Gig2341 <danmata2341@gmail.com>
2022-12-20 10:01:32 +0000

    advanced tasks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+	answer = 0
 	try:
 		answer = a / b
 		return (answer)
 	except ZeroDivisionError:
+		answer = None
 		pass
 	finally:
-	 	print("Inside result: {:f}".format(answer))
+	 	print("Inside result: {}".format(answer))
