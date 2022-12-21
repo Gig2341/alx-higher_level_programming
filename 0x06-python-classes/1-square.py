a80a5076079d1a8f43e5ec5a42aed089ba8c23de
Gig2341 <danmata2341@gmail.com>
2022-12-21 11:44:10 +0000

    documentation for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # 0-square.py by Frempong Daniel
 
+
 class Square:
 	''' a class Square that defines a square'''
 					    
 	def __init__(self, size):
 		'''initializes the square class
 		   Args: size - represents the size of the square'''
+
+
 		self.__size = size
