7ab5436e815c4a56c2d7f6b928b024f1ac879a30
Gig2341 <danmata2341@gmail.com>
2023-06-05 20:37:53 +0000

    mandatory tasks done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -108,3 +108,19 @@ class Rectangle:
 
 		print("Bye rectangle...")
 		Rectangle.number_of_instances -= 1
+
+	@staticmethod
+	def bigger_or_equal(rect_1, rect_2):
+		"""
+		this method returns the biggest rectangle based on the area
+		"""
+
+		if not isinstance(rect_1, Rectangle):
+			raise TypeError("rect_1 must be an instance of Rectangle")
+		if not isinstance(rect_2, Rectangle):
+			raise TypeError("rect_2 must be an instance of Rectangle")
+		if rect_2.area() > rect_1.area():
+			return rect_2
+		else:
+			return rect_1
+
