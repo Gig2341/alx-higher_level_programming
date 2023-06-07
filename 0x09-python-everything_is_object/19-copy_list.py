38627246f2d8c73ed2467d802d022915837ab69a
Gig2341 <danmata2341@gmail.com>
2023-06-07 20:05:06 +0000

    task 19 fixed

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+
+"""
+this module returns a copy of a list
+"""
+
+
 def copy_list(l):
-	new_list = list(l)
-	return new_list
+    """
+    this function returns a copy of a list
+    Args:
+        l = list to be copied
+    """
+    new_list = list(l)
+    return new_list
