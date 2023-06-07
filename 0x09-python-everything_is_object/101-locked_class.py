9216d8c7108352baef93b17c96bbe799fd2ecdb1
Gig2341 <danmata2341@gmail.com>
2023-06-07 21:31:57 +0000

    corrections done

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+this module defines a locked class
+"""
+
+
+class LockedClass:
+    """
+    this class only allows instantiation of an attribute called 
+    first_name
+    """
+    
+    __slots__ = ["first_name"]
