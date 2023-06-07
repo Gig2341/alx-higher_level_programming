ceff59406f19c7d226ce09aed8fa2726f1cb5447
Gig2341 <danmata2341@gmail.com>
2023-06-07 19:35:52 +0000

    pycodestyle errors corrected

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -71,6 +71,6 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             perimeter = 0
-	else:
-	    p = (self.__height + self.__width) * 2
+        else:
+            p = (self.__height + self.__width) * 2
         return p
