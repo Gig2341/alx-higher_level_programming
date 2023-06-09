2898d9d9e979f0cf26e173f5d706cce8c0329255
Gig2341 <danmata2341@gmail.com>
2023-06-09 23:46:28 +0000

    test file for add integer file fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+
 """
 this module contains a fuction that adds two numbers
 """
 
 def add_integer(a, b=98):
-	"""
-	"""
+    """
+    """
 
-	if not isinstance(a, int) and not isinstance(a, float):
-		raise TypeError("a must be an integer")
-	if not isinstance(b, int) and not isinstance(b, float):
-		raise TypeError("b must be an integer")
-	
-	return (a + b)
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return (a + b)
