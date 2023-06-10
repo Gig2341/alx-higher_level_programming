c610358259d554b23e4826c54675d6d30aa8771c
Gig2341 <danmata2341@gmail.com>
2023-06-10 00:04:36 +0000

    4 print file fixed file fixed

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,13 +1,19 @@
-def print_square(size):
-	"""
-	"""
+#!/usr/bin/python3
 
-	if size < 0:
-		raise ValueError("size must be >= 0")
-	if type(size) is not int:
-		raise TypeError("size must be an integer")
-	if type(size) is not float and size < 0:
-		raise TypeError("size must be an integer")
-	
-	for i in range(size):
-		print("#" * size)
+"""
+this module prints a square in '#'
+"""
+
+def print_square(size):
+    """
+    """
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is not float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        print("#" * size)
