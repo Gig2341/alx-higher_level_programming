aff2bb2a8ad769b8a4d4e18c66c951cf472457f0
Gig2341 <danmata2341@gmail.com>
2023-06-19 21:35:34 +0000

    unittest fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,14 +8,14 @@ this module divides a matrix
 def matrix_divided(matrix, div):
     """
     this function divides a matrix by a integer
-    
+
     Return: a matrix divided by div
-    Raises: 
+    Raises:
         TypeError: if div is not a number or matrix is not valid
         ZeroDivisionError : if divided by zero
     """
 
-    if not matrix or not isinstance (matrix, list):
+    if not matrix or not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (lists of lists) of integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
@@ -31,6 +31,6 @@ def matrix_divided(matrix, div):
         for column in row:
             if type(column) is not int and type(column) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    new_matrix = list(map(lambda x : list(map(lambda y : round(y/div, 2),
+    new_matrix = list(map(lambda x: list(map(lambda y: round(y/div, 2),
     x)), matrix))
     return new_matrix
